0420d57a6ce8747119a3786428215d65ac0cb5b5
unknown <ko9493@cu.ac.kr>
2022-10-12 22:33:30 +0900

    new : OCR and execution time check
    Modify Result Activity
    Remove Ingredient Labeling Activity
    Added server sending/receiving time return function
    Add temporary gallery icon

--- app/src/main/java/com/example/roicamera/ResultActivity.java
+++ app/src/main/java/com/example/roicamera/ResultActivity.java
@@ -3,97 +3,44 @@ package com.example.roicamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class ResultActivity extends AppCompatActivity {
     TextView img_text;
     String details;
 
-    TextView product1;
-    TextView product2;
-    TextView product3;
-    TextView product4;
-
-    String details1;
-    String details2;
-    String details3;
-    String details4;
-
-
-    String p1_detail;
-    String p2_detail;
-    String p3_detail;
-    String p4_detail;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        product1=(TextView)findViewById(R.id.p_details1);
-        product2=(TextView)findViewById(R.id.p_details2);
-        product3=(TextView)findViewById(R.id.p_details3);
-        product4=(TextView)findViewById(R.id.p_details4);
         img_text=(TextView)findViewById(R.id.img_details);
 
         Intent intent = getIntent();
 
-        details1 = intent.getExtras().getString("P1");
-        details2 = intent.getExtras().getString("P2");
-        details3 = intent.getExtras().getString("P3");
-        details4 = intent.getExtras().getString("P4");
         details = intent.getExtras().getString("text");
 
-        product1.setText(details1);
-        product2.setText(details2);
-        product3.setText(details3);
-        product4.setText(details4);
         img_text.setText(details);
-
-        p1_detail = intent.getExtras().getString("cosmetic1");
-        p2_detail = intent.getExtras().getString("cosmetic2");
-        p3_detail = intent.getExtras().getString("cosmetic3");
-        p4_detail = intent.getExtras().getString("cosmetic4");
-
-        product1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),IngredientActivity.class);
-                intent.putExtra("Ingredients", p1_detail);
-                startActivity(intent);
-            }
-        });
-        product2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),IngredientActivity.class);
-                intent.putExtra("Ingredients", p2_detail);
-                startActivity(intent);
-            }
-        });
-        product3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),IngredientActivity.class);
-                intent.putExtra("Ingredients", p3_detail);
-                startActivity(intent);
-            }
-        });
-        product4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),IngredientActivity.class);
-                intent.putExtra("Ingredients", p4_detail);
-                startActivity(intent);
-            }
-        });
 
     }
 
--- app/src/main/java/com/example/roicamera/RoiActivity.java
+++ app/src/main/java/com/example/roicamera/RoiActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -32,6 +33,10 @@ public class RoiActivity extends AppCompatActivity {
     Bitmap image;
 
     String roi_json = "";
+
+    String ocr;
+    String elapsed_time;
+    String time = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -71,22 +76,6 @@ public class RoiActivity extends AppCompatActivity {
 
     }
 
-    public class Cosmetic implements Serializable{
-        private static final long serialVersionUID = 1L;
-        private String rank;
-        private String brand;
-        private String name;
-        private String ingredients;
-
-
-        public Cosmetic(String rank, String brand, String name, String ingredients){
-            this.rank = rank;
-            this.brand = brand;
-            this.name = name;
-            this.ingredients = ingredients;
-        }
-    }
-
     public static String convertString(String val) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < val.length(); i++) {
@@ -107,19 +96,10 @@ public class RoiActivity extends AppCompatActivity {
         String text="";
         String text2="";
         String page = "";
-        String time = "";
+//        String ocr;
+//        String elapsed_time;
+//        String time = "";
 
-        String p1;
-        String p2;
-        String p3;
-        String p4;
-        String ingredient = "";
-
-
-        String p1_ingredient;
-        String p2_ingredient;
-        String p3_ingredient;
-        String p4_ingredient;
 
         Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
 
@@ -195,81 +175,26 @@ public class RoiActivity extends AppCompatActivity {
             text2 = convertString(page);
             JSONObject jObject = new JSONObject(text2);
 
+            ocr = jObject.getString("name");
+            elapsed_time = jObject.getString("elapsed_time");
 
-            String nproduct;
+            text += "OCR Result : \n" + ocr + "\n\n" + "Elapsed Time(OCR) : \n" + elapsed_time + "(s)" + "\n\n";
 
-            for(int i = 0; i < 4; i++){
-                String count = Integer.toString(i);
-                nproduct = jObject.getString(count);
-
-                JSONObject products = new JSONObject(nproduct);
-                String inproducts = products.getString("products");
-
-                JSONObject product = new JSONObject(inproducts);
-                String similarity = product.getString("similarity");
-
-                String mainproduct = product.getString("mainProduct");
-
-                JSONObject inmainproduct = new JSONObject(mainproduct);
-                String brand = inmainproduct.getString("brand");
-                String productname = inmainproduct.getString("productName");
-
-                JSONArray arr_ingredients = inmainproduct.optJSONArray("ingredients");
-                JSONObject element;
-
-                for( int j = 0; j < arr_ingredients.length(); j++){
-//                    element = (JSONObject) arr_ingredients.opt(i);
-                    JSONObject in_ingredients = arr_ingredients.getJSONObject(j);
-                    ingredient += "- ";
-//                    ingredient += element.optString("english");
-                    ingredient += in_ingredients.getString("english");
-                    if(j < arr_ingredients.length() - 1 ){
-                        ingredient += "\n---------------------------------------------------------------\n";
-                    }
-                }
-
-                Cosmetic[] cosmetic = new Cosmetic[4];
-
-                cosmetic[i] = new Cosmetic(similarity, brand, productname, ingredient);
-
-                switch(i){
-                    case 0:
-                        p1 = "Similarity : " + cosmetic[0].rank + "\n" + "Brand : " + cosmetic[0].brand + "\n" + "Product Name : " + cosmetic[0].name;
-                        p1_ingredient = "Brand : " + cosmetic[0].brand + "\n" + "Product Name : " + cosmetic[0].name + "\n\n\n" + "Ingredients : \n\n" + cosmetic[0].ingredients;
-                        intent.putExtra("P1", p1);
-                        intent.putExtra("cosmetic1", p1_ingredient);
-                        break;
-                    case 1:
-                        p2 = "Similarity : " + cosmetic[1].rank + "\n" + "Brand : " + cosmetic[1].brand + "\n" + "Product Name : " + cosmetic[1].name;
-                        p2_ingredient = "Brand : " + cosmetic[1].brand + "\n" + "Product Name : " + cosmetic[1].name + "\n\n\n" + "Ingredients : \n\n" + cosmetic[1].ingredients;
-                        intent.putExtra("P2", p2);
-                        intent.putExtra("cosmetic2", p2_ingredient);
-                        break;
-                    case 2:
-                        p3 = "Similarity : " + cosmetic[2].rank + "\n" + "Brand : " + cosmetic[2].brand + "\n" + "Product Name : " + cosmetic[2].name;
-                        p3_ingredient = "Brand : " + cosmetic[2].brand + "\n" + "Product Name : " + cosmetic[2].name + "\n\n\n" + "Ingredients : \n\n" + cosmetic[2].ingredients;
-                        intent.putExtra("P3", p3);
-                        intent.putExtra("cosmetic3", p3_ingredient);
-                        break;
-                    case 3:
-                        p4 = "Similarity : " + cosmetic[3].rank + "\n" + "Brand : " + cosmetic[3].brand + "\n" + "Product Name : " + cosmetic[3].name;
-                        p4_ingredient = "Brand : " + cosmetic[3].brand + "\n" + "Product Name : " + cosmetic[3].name + "\n\n\n" + "Ingredients : \n\n" + cosmetic[3].ingredients;
-                        intent.putExtra("P4", p4);
-                        intent.putExtra("cosmetic4", p4_ingredient);
-                        break;
-                }
-
-
-            }
-
-
+            //JSON파일 파싱 끝난 시간 기준 앱 처리 시간
             long lTime = endTime - startTime;
-            text += "Time Check : " + lTime/1000000000.0 + "(s)";
+            time += lTime/1000000000.0;
+            text += "Elapsed Time(App)  : " + lTime/1000000000.0 + "(s)";
+
+            intent.putExtra("name", ocr);
+            intent.putExtra("elapsed_time_ocr", elapsed_time);
+            intent.putExtra("elapsed_time_app", time);
 
             intent.putExtra("text", text);
 
-//            intent.putExtra("time", time);
-
+            new Thread(() -> {
+                    roi_json = MakeJson();
+                    UploadFile(roi_json);
+                }).start();
 
         } catch (MalformedURLException e) { // for URL.
             text=text+"Malformed URL Exception : \n"+e;
@@ -286,6 +211,77 @@ public class RoiActivity extends AppCompatActivity {
         }
 
         startActivity(intent);
+    }
+
+    public String MakeJson()  {
+        String json = "";
+
+        JSONObject app_info = new JSONObject();
+        try{
+            app_info.put("name", ocr);
+            app_info.put("elapsed_time_ocr", elapsed_time);
+            app_info.put("elapsed_time_app", time);
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        json = app_info.toString();
+        return json;
+    }
+
+    public void UploadFile(String string){
+        HttpURLConnection con = null;
+
+        String text2="";
+
+        try {
+            URL url = new URL("http:// your ip : port /api");
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+
+            con = (HttpURLConnection)url.openConnection();
+            con.setDoInput(true); //input 허용
+            con.setDoOutput(true);  // output 허용
+            con.setUseCaches(false);   // cache copy를 허용하지 않는다.
+            con.setRequestMethod("POST");
+            con.setConnectTimeout(10000);
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            // write data
+            DataOutputStream dos = new DataOutputStream(con.getOutputStream());
+            byte[] input = string.getBytes("UTF-8");
+
+            //Json파일 전송시 파라메터명은 time 파일명은 test.json으로 설정하여 전송송
+            dos.writeBytes("Content-Disposition: form-data; name=\"time\";filename=\"test.json\""
+                    + lineEnd);
+
+            dos.writeBytes(lineEnd);
+            dos.write(input);
+            dos.writeBytes(lineEnd);
+
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            dos.flush(); // finish upload...
+            dos.close();
+
+            if (con.getResponseCode() != HttpURLConnection.HTTP_OK){
+                text2+="Http URL not connected!\n";
+            }
+
+        } catch (MalformedURLException e) { // for URL.
+            text2=text2+"Malformed URL Exception : \n"+e;
+            Toast.makeText(getApplicationContext(), text2, Toast.LENGTH_LONG).show();
+        } catch (IOException e) { // for openConnection().
+            text2=text2+"IO Exception : \n"+e;
+            Toast.makeText(getApplicationContext(), text2, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            text2=text2+"Exception : \n"+e;
+            Toast.makeText(getApplicationContext(), text2, Toast.LENGTH_LONG).show();
+        } finally {
+            if (con != null)
+                con.disconnect();
+        }
+
     }
 
 }
